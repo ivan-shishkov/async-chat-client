24f6698282ea73ad335d000abe77a6ce3fda0ab2
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-05-26 15:36:59 +0300

    Added getting of command line arguments

--- chat_client.py
+++ chat_client.py
@@ -1,6 +1,8 @@
 import asyncio
 import time
 
+import configargparse
+
 import gui
 
 
@@ -10,7 +12,57 @@ async def generate_messages(queue):
         await asyncio.sleep(1)
 
 
+def get_command_line_arguments():
+    parser = configargparse.ArgumentParser()
+
+    parser.add_argument(
+        '--host',
+        help='Host for connect to chat. Required',
+        env_var='CHAT_HOST',
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        '--read-port',
+        help='Port for connect to chat for reading messages. Default: 5000',
+        env_var='CHAT_READ_PORT',
+        type=int,
+        default=5000,
+    )
+    parser.add_argument(
+        '--write-port',
+        help='Port for connect to chat for writing messages. Default: 5050',
+        env_var='CHAT_WRITE_PORT',
+        type=int,
+        default=5050,
+    )
+    parser.add_argument(
+        '--token',
+        help='User token for authorisation in chat.',
+        env_var='CHAT_AUTH_TOKEN',
+        type=str,
+        default='',
+    )
+    parser.add_argument(
+        '--nickname',
+        help='User nickname for registering in chat.',
+        env_var='CHAT_NICKNAME',
+        type=str,
+        default='',
+    )
+    parser.add_argument(
+        '--output',
+        help='Filepath for save chat messages. Default: chat.txt',
+        env_var='OUTPUT_FILEPATH',
+        type=str,
+        default='chat.txt',
+    )
+    return parser.parse_args()
+
+
 async def main():
+    command_line_arguments = get_command_line_arguments()
+
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
