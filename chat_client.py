7d0c34d95873357278716ae0addc0dad654b0622
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-05-26 16:03:49 +0300

    Added reading chat messages

--- chat_client.py
+++ chat_client.py
@@ -1,15 +1,49 @@
 import asyncio
-import time
+import socket
+from contextlib import asynccontextmanager
 
 import configargparse
 
 import gui
 
 
-async def generate_messages(queue):
+@asynccontextmanager
+async def open_connection(host, port):
+    reader, writer = await asyncio.open_connection(host=host, port=port)
+    try:
+        yield reader, writer
+    finally:
+        writer.close()
+
+
+async def read_messages(
+        host, port, queue,
+        connection_attempts_count_without_timeout=2,
+        timeout_between_connection_attempts=3):
+    current_connection_attempt = 0
+
     while True:
-        queue.put_nowait(f'Ping {int(time.time())}')
-        await asyncio.sleep(1)
+        try:
+            current_connection_attempt += 1
+
+            async with open_connection(host=host, port=port) as (reader, writer):
+                current_connection_attempt = 0
+
+                queue.put_nowait('Connection established')
+
+                while True:
+                    message = await reader.readline()
+                    queue.put_nowait(f'{message.decode().strip()}')
+
+        except (socket.gaierror, ConnectionRefusedError, ConnectionResetError):
+            if current_connection_attempt < connection_attempts_count_without_timeout:
+                queue.put_nowait('No connection. Retrying.')
+            else:
+                queue.put_nowait(
+                    f'No connection. '
+                    f'Retrying in {timeout_between_connection_attempts} sec.',
+                )
+                await asyncio.sleep(timeout_between_connection_attempts)
 
 
 def get_command_line_arguments():
@@ -63,13 +97,24 @@ def get_command_line_arguments():
 async def main():
     command_line_arguments = get_command_line_arguments()
 
+    chat_host = command_line_arguments.host
+    chat_read_port = command_line_arguments.read_port
+
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
 
     await asyncio.gather(
-        gui.draw(messages_queue, sending_queue, status_updates_queue),
-        generate_messages(messages_queue),
+        gui.draw(
+            messages_queue=messages_queue,
+            sending_queue=sending_queue,
+            status_updates_queue=status_updates_queue,
+        ),
+        read_messages(
+            host=chat_host,
+            port=chat_read_port,
+            queue=messages_queue,
+        ),
     )
 
 
