eec4d81ce12b3ec2faabf043e280213e349cdaa8
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-06-10 14:48:24 +0300

    Added prevent of sending empty messages

--- gui_chat_client.py
+++ gui_chat_client.py
@@ -35,8 +35,10 @@ class NicknameReceived:
 
 def process_new_message(input_field, sending_queue):
     text = input_field.get()
-    sending_queue.put_nowait(text)
-    input_field.delete(0, tk.END)
+
+    if text:
+        sending_queue.put_nowait(text)
+        input_field.delete(0, tk.END)
 
 
 async def update_tk(root_frame, interval=1 / 120):
