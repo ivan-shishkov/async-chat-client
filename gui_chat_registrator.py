0c54441e66774b3408c549ae4b4fac2954895415
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-06-11 10:14:05 +0300

    Added placement of window to screen center for better usability

--- gui_chat_registrator.py
+++ gui_chat_registrator.py
@@ -24,10 +24,32 @@ async def update_tk(root_frame, interval=1 / 120):
         await asyncio.sleep(interval)
 
 
+def get_window_size(window):
+    geometry_info = window.geometry().split('+')
+    size_info = geometry_info[0].split('x')
+    window_width = int(size_info[0])
+    window_height = int(size_info[1])
+
+    return window_width, window_height
+
+
+def set_window_to_center_screen(window):
+    window_width, window_height = get_window_size(window)
+
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    window.geometry(
+        f'+{screen_width // 2 - window_width // 2}'
+        f'+{screen_height // 2 - window_height // 2}',
+    )
+
+
 async def draw(nickname_queue):
     root = tk.Tk()
 
     root.title('Minecraft Chat Registrator')
+    root.resizable(False, False)
 
     root_frame = tk.Frame()
     root_frame.pack(fill='both', expand=True)
@@ -44,5 +66,9 @@ async def draw(nickname_queue):
     send_button['text'] = 'Register'
     send_button['command'] = lambda: process_nickname(input_field, nickname_queue)
     send_button.pack(side='top', ipady=10, pady=10)
+
+    root.update_idletasks()
+
+    set_window_to_center_screen(root)
 
     await update_tk(root_frame)
